aa2ac2eea7435bc67405194e020c41f658dd8164
Mor <mortogo321@gmail.com>
2023-10-22 15:20:40 +0700

    blog model

--- app/database/sqlite.py
+++ app/database/sqlite.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(
-    env("SQLALCHEMY_DATABASE_URL"), connect_args={"check_same_thread": False}
+    env["SQLALCHEMY_DATABASE_URL"], connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.database.sqlite import Base, engine
 from app.models.baseModel import Response
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
